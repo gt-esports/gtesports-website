c06560553648d8d21c05b3100321e922ae01f9cb
dbunnik3 <dbunnik3@gatech.edu>
2023-07-12 15:56:26 -0400

    Change card layout.

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -27,6 +27,42 @@ function Games() {
       icon: "/lol_logo.png",
       discord: "https://discord.gg/T3vB5jYhdE",
     },
+    {
+      name: "League of Legends",
+      desc: "This is the League of Legends description blah blah blah blah blah.",
+      icon: "/lol_logo.png",
+      discord: "https://discord.gg/T3vB5jYhdE",
+    },
+    {
+      name: "League of Legends",
+      desc: "This is the League of Legends description blah blah blah blah blah. This is the League of Legends description blah blah blah blah blah.",
+      icon: "/lol_logo.png",
+      discord: "https://discord.gg/T3vB5jYhdE",
+    },
+    {
+      name: "League of Legends",
+      desc: "This is the League of Legends description blah blah blah blah blah.",
+      icon: "/lol_logo.png",
+      discord: "https://discord.gg/T3vB5jYhdE",
+    },
+    {
+      name: "League of Legends",
+      desc: "This is the League of Legends description blah blah blah blah blah.",
+      icon: "/lol_logo.png",
+      discord: "https://discord.gg/T3vB5jYhdE",
+    },
+    {
+      name: "League of Legends",
+      desc: "This is the League of Legends description blah blah blah blah blah. This is the League of Legends description blah blah blah blah blah.",
+      icon: "/lol_logo.png",
+      discord: "https://discord.gg/T3vB5jYhdE",
+    },
+    {
+      name: "League of Legends",
+      desc: "This is the League of Legends description blah blah blah blah blah.",
+      icon: "/lol_logo.png",
+      discord: "https://discord.gg/T3vB5jYhdE",
+    },
   ];
 
   return (
@@ -34,7 +70,7 @@ function Games() {
       <h1 className="py-6 text-center text-5xl font-bold text-light-purple">
         Games
       </h1>
-      <div className="flex flex-col justify-center px-20 md:flex-row">
+      <div className="flex flex-col justify-center gap-8 px-20 md:mx-auto md:w-11/12 md:flex-row md:flex-wrap md:justify-evenly">
         {games.map((game, index) => {
           return <GameCard key={index} game={game} />;
         })}
