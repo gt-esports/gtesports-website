e380f4cc868db890ad4c84f0bae88f8592318245
rmmhm <ryanma22303@gmail.com>
2023-12-15 15:32:39 -0500

    set up background

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,13 @@
 function Home() {
-  return <div>This is the home page.</div>;
+  return (
+    <div className="h-screen w-full">
+      <div className="h-full w-full bg-home-1 bg-cover"></div>
+      <div
+        className="w-full bg-streak bg-cover"
+        style={{ height: "200vh" }}
+      ></div>
+    </div>
+  );
 }
 
 export default Home;
