7c18700b6450df6df11cbfd19ab0bff4b3c6fbdc
rmmhm <ryanma22303@gmail.com>
2023-12-15 15:39:40 -0500

    crop background img

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,7 @@ function Home() {
   return (
     <div className="h-screen w-full">
       <div className="h-full w-full bg-home-1 bg-cover"></div>
-      <div
-        className="w-full bg-streak bg-cover"
-        style={{ height: "200vh" }}
-      ></div>
+      <div className="h-[200vh] w-full bg-streak bg-cover"></div>
     </div>
   );
 }
