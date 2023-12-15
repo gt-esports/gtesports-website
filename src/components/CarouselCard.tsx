628f2a2eb03a53c1ac7b7356bfc68ac867849517
rmmhm <ryanma22303@gmail.com>
2023-12-15 16:33:29 -0500

    start game carousel

--- src/components/CarouselCard.tsx
+++ src/components/CarouselCard.tsx
@@ -1,12 +1,17 @@
 interface CarouselCardProps {
   image: string;
+<<<<<<< HEAD
   name: string;
   link: string;
   center: boolean;
+=======
+  link: string;
+>>>>>>> 1f60b4b (start game carousel)
 }
 
 function CarouselCard(props: CarouselCardProps) {
   return (
+<<<<<<< HEAD
     <div
       className={`h-full w-full pl-10 pr-10 text-center transition-transform duration-300 ${
         props.center ? "scale-100" : "scale-75"
@@ -16,6 +21,11 @@ function CarouselCard(props: CarouselCardProps) {
         <img src={props.image} className="h-full w-full object-cover" />
       </a>
       <p className="text-white">{props.name}</p>
+=======
+    <div>
+      <img src={props.image} />
+      <p>{props.link}</p>
+>>>>>>> 1f60b4b (start game carousel)
     </div>
   );
 }
