590c1530797da0aaf51d10b01c55e940c737efd4
ericlim9oo <ericlim316@gmail.com>
2023-12-17 01:54:48 -0500

    added pages with routing, stylized the navbar, changed tab logo to GT

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
 function Home() {
-  return <div>This is the home page.</div>;
+  return (
+    <div className="h-screen w-full">
+      <div className="flex h-full w-full items-center justify-center bg-home-1 bg-cover ">
+        <h1 className="font-bayon text-9xl font-normal text-tech-gold">
+          G<span className="text-white">eorgi</span>a Tech{" "}
+          <span className="text-white">Esports</span>
+        </h1>
+      </div>
+    </div>
+  )
 }
 
 export default Home;
