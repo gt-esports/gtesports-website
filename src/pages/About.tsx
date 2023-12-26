3b7d4fe9131e1fb39f3ab4f4714cdea38858b1f3
rmmhm <ryanma22303@gmail.com>
2023-12-26 16:58:27 -0500

    lint

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,7 @@
 function About() {
   return (
     <div className="h-screen w-full">
-      <div className="flex h-full w-full bg-streak bg-cover text-white pt-20">
+      <div className="flex h-full w-full bg-streak bg-cover pt-20 text-white">
         <a>This is the about page.</a>
       </div>
     </div>
--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,7 +1,7 @@
 function Games() {
   return (
     <div className="h-screen w-full">
-      <div className="flex h-full w-full bg-streak bg-cover text-white pt-20">
+      <div className="flex h-full w-full bg-streak bg-cover pt-20 text-white">
         <a>This is the game page.</a>
       </div>
     </div>
@@ -9,4 +9,3 @@ function Games() {
 }
 
 export default Games;
-  
