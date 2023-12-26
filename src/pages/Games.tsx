6b321277237c22a8ad21017a650ff260864bf5d1
Eric <ericlim316@gmail.com>
2023-12-25 22:24:51 -0500

    created game cards

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,15 +1,17 @@
+import Carousel from "../components/Carousel";
+
 function Games() {
   return (
     <div className="h-screen w-full">
       <div className="flex h-[300vh] w-full flex-col items-center bg-streak bg-cover pt-36">
         <div>
-          <h2 className="font-bayon text-7xl tracking-wide text-white">
+          <h2 className="font-bayon text-7xl tracking-wide git text-white">
             GAMES
           </h2>
         </div>
-      </div>
-      <div className="mx-auto mt-5 flex w-4/5 max-w-screen-xl flex-col">
-        <a>yoooooooooo</a>
+        <div className="mx-auto mt-5 flex w-4/5 max-w-screen-xl flex-col text-white">
+          <Carousel></Carousel>
+        </div>
       </div>
     </div>
   );
