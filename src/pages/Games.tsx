c448eff892fe00c4c37b6f1be319a3764e10a0a8
Eric <ericlim316@gmail.com>
2024-02-03 00:57:38 -0500

    game page created and game cards partially complete: abort q y

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -5,7 +5,7 @@ function Games() {
   // const items = ['Item 1', 'Item 2', 'Item 3'];
 
   return (
-    <div className="flex flex-col w-full bg-streak bg-cover pt-36">
+    <div className="flex flex-colg w-full bg-streak bg-cover pt-36">
       <div className="flex items-center justify-center">
         <h1 className="font-bayon text-7xl tracking-wide text-white">
           GAMES
