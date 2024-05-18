c0ce1c039b6de8c4d08c28507c4a354617b8309e
Jim Chen <loxijchen@gmail.com>
2024-05-18 15:30:41 -0500

    fix gameGrid padding

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -7,7 +7,7 @@ function Games() {
       <div className="flex items-center justify-center">
         <h1 className="justify-center font-bayon text-6xl text-white">GAMES</h1>
       </div>
-      <div className="flex flex-col items-center py-16 pb-28">
+      <div className="flex flex-col items-center pb-40 pt-4">
         <GameGrid />
       </div>
       <Footer />
