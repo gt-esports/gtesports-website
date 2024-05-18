11af132ad8f3cc6d1eb370bb0096ff3ebc91bc50
Jim Chen <loxijchen@gmail.com>
2024-05-18 16:19:52 -0500

    complete static sections of recruitment card, need to add link to learn more button

--- src/components/RecruitmentCard.tsx
+++ src/components/RecruitmentCard.tsx
@@ -2,34 +2,45 @@ import { RecruitmentCardProps } from "../types";
 
 export default function RecruitmentCard(props: RecruitmentCardProps) {
   return (
-    <div className="items flex w-9/12 flex-row rounded-xl bg-gradient-to-br from-[#3d3d3d]/90 to-[#a7a7a7]/90">
+    <div className="items flex w-9/12 flex-row rounded-xl bg-gradient-to-br from-[#3d3d3d]/95 to-[#a7a7a7]/95">
       <img
         src={props.image}
         alt={`${props.name}`}
-        className="card-image m-2 h-[260px] w-[195px] rounded-2xl pb-2 pl-3 pr-3 pt-3"
+        className="card-image m-2 h-[280px] w-[195px] rounded-2xl pb-2 pl-3 pr-3 pt-3"
       />
-      <div className="flex flex-col pt-4 tracking-wide">
-        <h1 className="pb-3 pt-6 font-bayon text-[1.4rem] tracking-wider text-bright-buzz">
+
+      {/* text info */}
+      <div className="tracking-wid flex w-full flex-col pl-1 pt-4">
+        <h1 className=" flex flex-row-reverse pr-8 font-barlow text-[0.95rem] font-semibold tracking-wide text-white">
+          Tryout date: 8/27/2023
+        </h1>
+        <h1 className="pb-2 font-bayon text-[1.4rem] tracking-wider text-bright-buzz">
           {props.name}
         </h1>
-        <p className="pt-2 font-barlow text-[1.1rem] tracking-wider text-bright-buzz">
+        <p className=" font-barlow text-[0.95rem] tracking-wider text-bright-buzz">
           Recruitment Info:
         </p>
-        <p className="tracking pl-3 font-barlow text-[0.95rem] font-light text-white">
+        <p className="tracking pl-3 font-barlow text-[0.9rem] font-light text-white">
           {props.recruitmentInfo}
         </p>
-
-        <p className="pt-2 font-barlow text-[1.05rem] tracking-wider text-bright-buzz">
+        <p className="pt-2 font-barlow text-[.95rem] tracking-wider text-bright-buzz">
           Contact:
         </p>
-
-        <p className="tracking pl-3 font-barlow text-[0.95rem] font-light text-white">
+        <p className="tracking pl-3 font-barlow text-[0.9rem] font-light text-white">
           • email: {props.contactEmail}
         </p>
-
         <p className="tracking pl-3 font-barlow text-[0.95rem] font-light text-white">
           • discord: {props.contactDiscord}
         </p>
+        {/* button here */}
+        <div className="mb-3 flex flex-row-reverse pr-4 pt-8">
+          <button className="learn-more-btn text-barlow ml-3 flex items-center justify-center tracking-wide">
+            Learn More
+          </button>
+        </div>
+        {/* <a href={props.discordLink} target="_blank">
+            <button className="discord-btn mr-3"></button>
+          </a> */}
       </div>
     </div>
   );
