85a894aa7a0bbec3b55c8b2f21dd8b54813eddfa
Jim Chen <loxijchen@gmail.com>
2024-10-06 12:28:51 -0400

    clean up for build

--- src/data/recruitmentData.ts
+++ src/data/recruitmentData.ts
@@ -1,27 +1,27 @@
 import league from "../assets/game-covers/league-of-legends-cover.png";
-import tft from "../assets/game-covers/teamfight-tactics-cover.jpg";
+// import tft from "../assets/game-covers/teamfight-tactics-cover.jpg";
 import valorant from "../assets/game-covers/valorant-cover.jpeg";
 import overwatch2 from "../assets/game-covers/overwatch-2-cover.jpeg";
-import csgo2 from "../assets/game-covers/counter-strike-2-cover.jpeg";
-import smash from "../assets/game-covers/smash-cover.jpeg";
-import r6 from "../assets/game-covers/rainbow-six-siege-cover.jpeg";
-import rythm from "../assets/game-covers/rythm-games-cover.jpeg";
-import tf2 from "../assets/game-covers/team-fortress-2-cover.jpeg";
-import hearthstone from "../assets/game-covers/hearthstone-cover.jpeg";
-import smite from "../assets/game-covers/smite-cover.jpeg";
-import rl from "../assets/game-covers/rocket-league-cover.jpeg";
-import cod from "../assets/game-covers/cod-cover.jpg";
-import apex from "../assets/game-covers/apex-cover.jpeg";
-import dota2 from "../assets/game-covers/dota-2-cover.jpeg";
+// import csgo2 from "../assets/game-covers/counter-strike-2-cover.jpeg";
+// import smash from "../assets/game-covers/smash-cover.jpeg";
+// import r6 from "../assets/game-covers/rainbow-six-siege-cover.jpeg";
+// import rythm from "../assets/game-covers/rythm-games-cover.jpeg";
+// import tf2 from "../assets/game-covers/team-fortress-2-cover.jpeg";
+// import hearthstone from "../assets/game-covers/hearthstone-cover.jpeg";
+// import smite from "../assets/game-covers/smite-cover.jpeg";
+// import rl from "../assets/game-covers/rocket-league-cover.jpeg";
+// import cod from "../assets/game-covers/cod-cover.jpg";
+// import apex from "../assets/game-covers/apex-cover.jpeg";
+// import dota2 from "../assets/game-covers/dota-2-cover.jpeg";
 
-import minecraft from "../assets/game-covers/minecraft-cover.jpeg";
-import genshin from "../assets/game-covers/genshin-impact-cover.jpg";
-import pokemonGo from "../assets/game-covers/pokemon-go-cover.jpg";
-import fortnite from "../assets/game-covers/fortnite-cover.jpg";
-import animalCrossing from "../assets/game-covers/animal-crossing-cover.jpg";
-import destiny from "../assets/game-covers/destiny-cover.jpg";
-import hearts_of_iron from "../assets/game-covers/hearts-of-iron-4-cover.jpg";
-import amongus from "../assets/game-covers/amongus.png";
+// import minecraft from "../assets/game-covers/minecraft-cover.jpeg";
+// import genshin from "../assets/game-covers/genshin-impact-cover.jpg";
+// import pokemonGo from "../assets/game-covers/pokemon-go-cover.jpg";
+// import fortnite from "../assets/game-covers/fortnite-cover.jpg";
+// import animalCrossing from "../assets/game-covers/animal-crossing-cover.jpg";
+// import destiny from "../assets/game-covers/destiny-cover.jpg";
+// import hearts_of_iron from "../assets/game-covers/hearts-of-iron-4-cover.jpg";
+// import amongus from "../assets/game-covers/amongus.png";
 
 export const recruitmentData = {
   "League of Legends Tryout": {
