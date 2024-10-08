1961838fb4da758a1a5b4554a197860c7b7ba40f
Jim Chen <loxijchen@gmail.com>
2024-10-08 14:20:06 -0400

    add axios fetch request

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 function About() {
+
+  const fetchAPI = async () => {
+    const response = await axios.get("http://localhost:8080/api");
+    console.log(response.data.about);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
   return (
     <div className="h-screen w-full">
       <div className="flex h-full w-full bg-streak bg-cover text-white">
