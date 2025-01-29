d07c2d51d64d93884784d445922bed57cd66bbfa
thekevinhu <22kevinhu@gmail.com>
2025-01-28 19:29:15 -0500

    Update React type definitions and refactor RecruitmentCard component layout

--- src/components/RecruitmentCard.tsx
+++ src/components/RecruitmentCard.tsx
@@ -2,52 +2,46 @@ import { RecruitmentCardProps } from "../types";
 
 export default function RecruitmentCard(props: RecruitmentCardProps) {
   return (
-    <div className=" items box-content flex w-9/12 flex-row rounded-xl bg-gradient-to-br from-[#3d3d3d]/95 to-[#a7a7a7]/95">
-      {/* game image + discord icon */}
-      <div className="relative box-content h-[280px] w-[225px] border-0 p-4">
+    <div className="flex w-auto h-full flex-row p-4 rounded-xl bg-gradient-to-br from-[#3d3d3d]/95 to-[#a7a7a7]/95">
+      {/* game image */}
+      <div className="border-0 p-4">
         <img
           src={props.image}
           alt={`${props.name}`}
-          className="card-image h-full w-full rounded-2xl "
+          className="hidden md:block card-image h-[280px] w-[225px] rounded-2xl"
         />
-        <div className="absolute right-4 top-4 box-content flex h-16 w-16 items-center justify-center rounded-md bg-footer-shadow">
-          <a href={props.discordLink} target="_blank">
-            <button className="discord-btn opacity-7 m-4 h-[37px] w-[37px]"></button>
-          </a>
+      </div>
+      <div className="flex-col">
+        {/* text info */}
+        <div className="flex flex-col h-auto w-full pl-1 tracking-wider">
+          <h1 className="flex flex-row-reverse pr-2 font-sans text-[0.97rem] font-semibold tracking-wide text-white">
+            Tryout date: {props.tryoutDate}
+          </h1>
+          <h1 className="pb-2 font-bayon text-[1.4rem] text-bright-buzz">
+            {props.name}
+          </h1>
+          <p className="font-barlow text-[0.95rem] text-bright-buzz">
+            Recruitment Info:
+          </p>
+          <p className="tracking pl-3 font-barlow text-[0.9rem] font-light text-white">
+            {props.recruitmentInfo}
+          </p>
+            <p className="pt-2 font-barlow text-[.95rem] text-bright-buzz">
+              Contact:
+            </p>
+          <p className="tracking pl-3 font-barlow text-[0.9rem] font-light text-white">
+            • email: {props.contactEmail}
+          </p>
+          <p className="tracking pl-3 font-barlow text-[0.95rem] font-light text-white">
+            • discord: {props.contactDiscord}
+          </p>  
         </div>
-      </div>
-      {/* text info */}
-      <div className="box-content flex h-full w-full flex-col pl-1 pt-4 tracking-wider">
-        <h1 className=" flex flex-row-reverse pr-8 font-sans text-[0.97rem] font-semibold tracking-wide text-white">
-          Tryout date: {props.tryoutDate}
-        </h1>
-        <h1 className="pb-2 font-bayon text-[1.4rem] text-bright-buzz">
-          {props.name}
-        </h1>
-        <p className=" font-barlow text-[0.95rem] text-bright-buzz">
-          Recruitment Info:
-        </p>
-        <p className="tracking pl-3 font-barlow text-[0.9rem] font-light text-white">
-          {props.recruitmentInfo}
-        </p>
-        <p className="pt-2 font-barlow text-[.95rem] text-bright-buzz">
-          Contact:
-        </p>
-        <p className="tracking pl-3 font-barlow text-[0.9rem] font-light text-white">
-          • email: {props.contactEmail}
-        </p>
-        <p className="tracking pl-3 font-barlow text-[0.95rem] font-light text-white">
-          • discord: {props.contactDiscord}
-        </p>
-        {/* button here */}
-        {/* <a href={props.discordLink} target="_blank">
-            <button className="discord-btn mr-3"></button>
-          </a> */}
-      </div>
-      <div className="relative mb-3 pr-6 pt-8">
-        <button className="learn-more-btn absolute bottom-0 right-6 ml-3 mr-4 mt-6 flex scale-90 scale-x-[1.2] items-center justify-center tracking-tighter">
-          Learn More
-        </button>
+        {/* learn more button */}
+        <div className="flex justify-center md:flex md:items-center md:justify-end md:pt-0 pt-4">
+          <button className="learn-more-btn flex scale-x-[1.1] items-center justify-center tracking-tighter">
+            Learn More
+          </button>  
+        </div>
       </div>
     </div>
   );
