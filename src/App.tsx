2fd70d7db8e29d93a9b9bd23510d3379098ffdc4
Ethan Loo Yu Teng <eloo6@gatech.edu>
2025-01-31 16:57:31 -0500

    removed changes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import Teams from "./pages/Teams";
 
 function App() {
   return (
-    <div className="flex min-h-screen flex-col w-full max-w-screen">
+    <div className="flex min-h-screen flex-col">
       <Navbar />
       <main>
         <Routes>
