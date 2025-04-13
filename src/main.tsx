4d4bbe824eafcd45df0a6df97b2f8b36c9d0c1e9
Jim Chen <loxijchen@gmail.com>
2025-04-13 14:21:05 -0400

    insert publishable key

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = "pk_live_Y2xlcmsuZ2F0ZWNoZXNwb3J0cy5jb20k";
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
