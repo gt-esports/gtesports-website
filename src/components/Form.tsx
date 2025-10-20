6f2a8eba2b6fc54e8bfd77077ac3509f8179a56f
KJ <kjk@skyhopper.net>
2025-10-20 18:30:11 -0400

    basic email subscribe form friont end

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -6,6 +6,7 @@ import bee from "../assets/bee.png";
 import emailjs from "@emailjs/browser";
 import SocialBar from "./SocialBar";
 import MobileSocialBar from "./MobileSocialBar";
+import { Link } from "react-router-dom";
 
 const Form: React.FC = () => {
   const formRef = useRef<HTMLFormElement>(null);
@@ -29,38 +30,110 @@ const Form: React.FC = () => {
         }
       );
   };
+  
+  return (
+    <FormOuter>
+      <FormInner formRef={formRef} sendEmail={sendEmail} />
+    </FormOuter>
+  );
+};
+
+export const SubscribeForm: React.FC = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const subscribe = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!formRef.current) return;
+  };
 
   return (
-    <div className="flex w-full items-center justify-center px-4 sm:px-0">
-      <div className="relative w-full overflow-hidden rounded-lg bg-gradient-to-b from-navy-blue to-tech-gold shadow-xl">
-        {/* Main content container with improved mobile layout */}
-        <div className="flex w-full flex-col p-6 sm:p-8 md:flex-row md:gap-8">
-          {/* Form Section - Full width on mobile */}
-          <div className="w-full font-quicksand md:w-1/2">
+    <FormOuter>
+      <SubscribeInner formRef={formRef} subscribe={subscribe} />
+    </FormOuter>
+  );
+};
+
+const SubscribeInner = ({formRef, subscribe} : {formRef: React.RefObject<HTMLFormElement>, subscribe: (e: React.FormEvent) => void}) => {
+  return (
+    <div className="relative w-full overflow-hidden rounded-lg bg-gradient-to-b from-navy-blue to-tech-gold shadow-xl">
+      {/* Main content container with improved mobile layout */}
+      <div className="flex w-full flex-col p-6 sm:p-8 md:flex-row md:gap-8">
+        {/* Form Section - Full width on mobile */}
+        <div className="w-full font-quicksand">
+          <h2 className="mb-6 text-2xl font-bold text-white">Subscribe to our Email Newsletter</h2>
+          <form
+            ref={formRef}
+            onSubmit={subscribe}
+            className="space-y-4 text-white"
+          >
+            <input
+              type="text"
+              name="first-name"
+              placeholder="Enter your first name"
+              className="w-full rounded-md border-navy-blue bg-navy-blue p-3 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+              required
+              />
+            <input
+              type="text"
+              name="last-name"
+              placeholder="Enter your last name"
+              className="w-full rounded-md border-navy-blue bg-navy-blue p-3 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email address"
+              className="w-full rounded-md border-navy-blue bg-navy-blue p-3 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+              required
+            />
+            <p className="text-gray-400 text-sm">
+              By filling out this form,  you agree to the terms & conditions in our <Link to="/privacy">Privacy Form</Link>
+            </p>
+            <button
+              type="submit"
+              className="w-full rounded-md bg-bright-buzz py-3 font-bold text-white shadow-md transition-colors hover:bg-yellow-700 focus:outline-none"
+            >
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const FormInner = ({formRef, sendEmail} : {formRef: React.RefObject<HTMLFormElement>, sendEmail: (e: React.FormEvent) => void}) => {
+  return (
+    <div className="relative w-full overflow-hidden rounded-lg bg-gradient-to-b from-navy-blue to-tech-gold shadow-xl">
+      {/* Main content container with improved mobile layout */}
+      <div className="flex w-full flex-col p-6 sm:p-8 md:flex-row md:gap-8">
+        {/* Form Section - Full width on mobile */}
+        <div className="w-full font-quicksand md:w-1/2">
             <h2 className="mb-6 text-2xl font-bold text-white">GET IN TOUCH</h2>
             <form
               ref={formRef}
               onSubmit={sendEmail}
-              className="space-y-4 text-black"
+              className="space-y-4 text-white"
             >
               <input
                 type="text"
                 name="name"
                 placeholder="Enter your name"
-                className="w-full rounded-md border bg-gray-100 p-3 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                className="w-full rounded-md border-navy-blue bg-navy-blue p-3 focus:outline-none focus:ring-2 focus:ring-yellow-400"
                 required
               />
               <input
                 type="email"
                 name="email"
                 placeholder="Enter your email address"
-                className="w-full rounded-md border bg-gray-100 p-3 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                className="w-full rounded-md border-navy-blue bg-navy-blue p-3 focus:outline-none focus:ring-2 focus:ring-yellow-400"
                 required
               />
               <textarea
                 name="message"
                 placeholder="Go ahead, we are listening..."
-                className="w-full rounded-md border bg-gray-100 p-3 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                className="w-full rounded-md border-navy-blue bg-navy-blue p-3 focus:outline-none focus:ring-2 focus:ring-yellow-400"
                 rows={4}
                 required
               />
@@ -111,6 +184,13 @@ const Form: React.FC = () => {
           <MobileSocialBar />
         </div>
       </div>
+    );
+};
+
+const FormOuter: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <div className="flex w-full items-center justify-center px-4 sm:px-0">
+      {children}
     </div>
   );
 };
