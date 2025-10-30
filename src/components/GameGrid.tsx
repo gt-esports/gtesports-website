11386351ebd7d28a12c8a4bc9b05afda8ea2216d
rixified <nishantdas88@gmail.com>
2025-10-30 16:08:20 -0400

    Revert "Merge branch 'main' into learn-more-feature"
    This reverts commit 454b4d066a49bbca638374283d7e2d2f791781f8, reversing
    changes made to 34c59ccaaad4d9b12f8375ee6667ac5b692e1756.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,16 +1,7 @@
-import { useState, useEffect, useMemo } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, EffectCoverflow, A11y } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/effect-coverflow";
 import GameCard from "./GameCard";
 import GameModal from "./GameModal";
 import { games, casual_games } from "../data/gamesData";
-import CarouselCard from "./CarouselCard";
-import SearchBar from "./SearchBar";
+import { useState } from "react";
 
 function GameGrid() {
   const [comp, setComp] = useState(true);
@@ -25,58 +16,31 @@ function GameGrid() {
   const handleCloseModal = () => {
     setModalOpen(false);
     setSelectedGame(null);
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
-  const [searchTerm, setSearchTerm] = useState("");
-  const lowerSearchTerm = searchTerm.toLowerCase();
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 1024);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  const gamesToDisplay = useMemo(() => Object.entries(comp ? games : casual_games).filter(([name]) => name.toLowerCase().includes(lowerSearchTerm)), [comp, lowerSearchTerm]);
-          
-  function handleFilter(input: string) {
-    setSearchTerm(input);
-  }
-
-  const animationVariants = {
-    initial: { opacity: 0, y: 20 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -20 },
   };
 
   return (
-    <div className="flex flex-wrap justify-center px-8 md:px-[100px] py-4 min-w-[400px]">
-      <div className="flex flex-col w-full justify-center pb-4 font-barlow text-xl 2xl:left-56">
-        <div className="flex justify-center w-full pb-4 font-barlow text-xl">
-          <button
-            className={
-              comp
-                ? "px-2 text-bright-buzz underline underline-offset-8"
-                : "px-2 text-white underline-offset-4 duration-500 hover:text-bright-buzz"
-            }
-            onClick={() => setComp(true)}
-          >
-            Competitive
-          </button>
-          <button
-            className={
-              !comp
-                ? "px-2 text-bright-buzz underline underline-offset-8"
-                : "px-2 text-white underline-offset-4 duration-500 hover:text-bright-buzz"
-            }
-            onClick={() => setComp(false)}
-          >
-            Casual
-          </button>
-        </div>
-        <div className="pb-4 flex justify-center">
-          <SearchBar searchTerm={searchTerm} placeholder={"Search games..."} handleInput={handleFilter} />
-        </div>
+    <div className="flex flex-wrap justify-center px-[100px]">
+      <div className="xl:right:24 flex justify-center w-full pb-4 font-barlow text-xl 2xl:left-56">
+        <button
+          className={
+            comp
+              ? "px-2 text-bright-buzz underline underline-offset-8"
+              : "px-2 text-white underline-offset-4 duration-500 hover:text-bright-buzz"
+          }
+          onClick={() => setComp(true)}
+        >
+          Competitive
+        </button>
+        <button
+          className={
+            !comp
+              ? "px-2 text-bright-buzz underline underline-offset-8"
+              : "px-2 text-white underline-offset-4 duration-500 hover:text-bright-buzz"
+          }
+          onClick={() => setComp(false)}
+        >
+          Casual
+        </button>
       </div>
 
       <div className="flex flex-wrap items-center justify-center">
@@ -94,67 +58,6 @@ function GameGrid() {
             </div>
           )
         )}
-      <div className="w-full">
-        <AnimatePresence mode="wait">
-          {isMobile ? (
-            <motion.div
-              key="mobile"
-              variants={animationVariants}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              transition={{ duration: 0.5 }}
-            >
-              <Swiper
-                effect={"coverflow"}
-                navigation={true}
-                centeredSlides={true}
-                modules={[Pagination, Navigation, EffectCoverflow, A11y]}
-                pagination={{ clickable: true }}
-                loop={false}
-                observer={true}
-                observeParents={true}
-                slidesPerView={1}
-                spaceBetween={0}
-                coverflowEffect={{ rotate: 50, stretch: 0, depth: 100, modifier: 1, slideShadows: true }}
-                className="w-full pb-12"
-              >
-                {gamesToDisplay.map(([name, game]) => (
-                  <SwiperSlide key={name} className="flex justify-center items-center">
-                    <CarouselCard image={game.image} name={name} link={game.discordLink} />
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </motion.div>
-          ) : (
-            <motion.div
-              key="desktop"
-              variants={animationVariants}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              transition={{ duration: 0.5 }}
-            >
-              <div className="flex flex-wrap items-center justify-center">
-                {gamesToDisplay.map(([name, game]) => (
-                  <motion.div
-                    layout
-                    transition={{ type: "spring", stiffness: 300, damping: 25 }}
-                    className="p-3"
-                    key={name}
-                  >
-                    <GameCard
-                      image={game.image}
-                      name={name}
-                      link={game.pageLink}
-                      discordLink={game.discordLink}
-                    />
-                  </motion.div>
-                ))}
-              </div>
-            </motion.div>
-          )}
-        </AnimatePresence>
       </div>
       <GameModal
         isOpen={modalOpen}
