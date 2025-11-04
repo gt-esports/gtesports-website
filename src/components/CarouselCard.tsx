4a73517c7bb2be531898733f8a0c15909cc2460c
GaoWilson81 <wilsongao2002@gmail.com>
2025-11-04 16:38:37 -0500

    Mobile optimized
    - fixed carousel to display full image.
    - mobile optimized
    - changed ‘GA Tech Esports’ to ‘Georgia Tech Esports'
    - removed gamefest button

--- src/components/CarouselCard.tsx
+++ src/components/CarouselCard.tsx
@@ -7,14 +7,21 @@ interface CarouselCardProps {
 function CarouselCard(props: CarouselCardProps) {
   return (
     <div
-      className={`align-center h-[490px] w-[420px] flex-col justify-center rounded-lg px-10 text-center
-      duration-300`}
+      className={`align-center flex w-full max-w-[420px] flex-col items-center justify-center rounded-lg px-4 text-center duration-300 xs:px-6 sm:px-8 md:px-10`}
+      style={{ overflow: 'visible' }}
     >
-      <a href={props.link} target="blank">
+      <a href={props.link} target="blank" className="w-full" style={{ overflow: 'visible' }}>
         <img
           src={props.image}
           loading="lazy"
-          className="h-full w-full rounded-lg object-cover"
+          className="w-full"
+          style={{ 
+            objectFit: 'contain',
+            maxHeight: '490px',
+            height: 'auto',
+            display: 'block'
+          }}
+          alt={props.name}
         />
       </a>
       <p className="py-4 font-barlow font-semibold tracking-wide text-white">
