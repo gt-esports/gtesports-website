5decc51c20788fefc7de88754651ddec627205e6
KJ <kjk@skyhopper.net>
2025-11-18 14:19:08 -0500

    remove clerk provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,19 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop.tsx";
-import { ClerkProvider } from "@clerk/clerk-react";
 
-const PUBLISHABLE_KEY = "pk_live_Y2xlcmsuZ2F0ZWNoZXNwb3J0cy5jb20k";
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
-}
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <BrowserRouter>
-        <ScrollToTop />
-        <App />
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter>
+      <ScrollToTop />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
