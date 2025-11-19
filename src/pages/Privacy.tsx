37dfe76557965eaba71b1443bea21812a5d58c7b
Ethan Loo Yu Teng <eloo6@gatech.edu>
2025-11-19 11:45:37 -0500

    Add Privacy Policy page and route

---
+++ src/pages/Privacy.tsx
@@ -0,0 +1,132 @@
+function Privacy() {
+  return (
+    <div className="flex flex-col min-h-screen w-full px-4 pt-[--navbar-height] sm:px-6 md:px-12 lg:px-16 xl:px-20 justify-center">
+      <h1 className="font-bayon text-tech-gold text-3xl">Privacy Policy
+        <br />
+        <span className='text-white text-lg'>Last Updated On 17-Nov-2025</span>
+        <br />
+        <span className='text-white text-lg'>Effective Date 17-Nov-2025</span>
+      </h1>
+
+      <p className="font-quicksand text-white">
+          This Privacy Policy describes the policies of
+
+          Georgia Tech Esports,
+          801 Atlantic Drive NW,
+          Georgia
+          30332,
+          United States of America (the),
+          email: georgiatechesports@gmail.com
+
+          on the collection,
+          use and disclosure of your information that we collect
+          when you use our website ( https://gatechesports.com/home ).
+          (the “Service”). By accessing or using
+          the
+          Service, you are consenting to the collection, use and
+          disclosure of your information in accordance with this
+          Privacy Policy. If you do not consent to the same,
+          please do not access or use the Service.
+      </p>
+
+      <p className="font-quicksand text-white">
+          We may modify this Privacy Policy at any time without
+          any prior notice to you and will post the revised
+          Privacy Policy on the Service. The revised Policy will
+          be effective 180 days from when the
+          revised Policy is posted in the Service and your
+          continued access or use of the Service after such time
+          will constitute your acceptance of the revised Privacy
+          Policy. We therefore recommend that you periodically
+          review this page.
+      </p>
+
+      <ol className="font-quicksand text-white space-y-4 py-4">
+          <li>
+              <h2 className="font-bayon text-tech-gold text-xl">
+                  Your Rights:
+              </h2>
+              <p>
+                  Depending on the law that applies, you may have
+                  a right to access and rectify or erase your
+                  personal data or receive a copy of your personal
+                  data, restrict or object to the active
+                  processing of your data, ask us to share (port)
+                  your personal information to another entity,
+                  withdraw any consent you provided to us to
+                  process your data, a right to lodge a complaint
+                  with a statutory authority and such other rights
+                  as may be relevant under applicable laws. To
+                  exercise these rights, you can write to us at
+                  georgiatechesports@gmail.com.
+                  We will respond to your
+                  request in accordance with applicable law.
+              </p>
+
+              
+              <p>
+                  Do note that if you do not allow us to collect
+                  or process the required personal information or
+                  withdraw the consent to process the same for the
+                  required purposes, you may not be able to access
+                  or use the services for which your information
+                  was sought.
+              </p>
+          </li>
+          
+          <li>
+              <h2 className="font-bayon text-tech-gold text-xl">
+                  Security:
+              </h2>
+              <p>
+                  The security of your information is important to
+                  us and we will use reasonable security measures
+                  to prevent the loss, misuse or unauthorized
+                  alteration of your information under our
+                  control. However, given the inherent risks, we
+                  cannot guarantee absolute security and
+                  consequently, we cannot ensure or warrant the
+                  security of any information you transmit to us
+                  and you do so at your own risk.
+              </p>
+          </li>
+
+          <li>
+              <h2 className="font-bayon text-tech-gold text-xl">
+                  Third Party Links & Use Of Your Information:
+              </h2>
+              <p>
+                  Our Service may contain links to other websites
+                  that are not operated by us. This Privacy Policy
+                  does not address the privacy policy and other
+                  practices of any third parties, including any
+                  third party operating any website or service
+                  that may be accessible via a link on the
+                  Service. We strongly advise you to review the
+                  privacy policy of every site you visit. We have
+                  no control over and assume no responsibility for
+                  the content, privacy policies or practices of
+                  any third party sites or services.
+              </p>
+          </li>
+          
+          <li>
+              <h2 className="font-bayon text-tech-gold text-xl">
+                  Grievance / Data Protection Officer:
+              </h2>
+              <p>
+                  If you have any queries or concerns about the
+                  processing of your information that is available
+                  with us, you may email our Grievance Officer at
+                  Georgia Tech Esports,
+                  801 Atlantic Drive NW,
+                  email: georgiatechesports@gmail.com.
+                  We will address your concerns in accordance with applicable law.
+              </p>
+          </li>
+      </ol>
+    </div>
+  );
+}
+
+export default Privacy;
